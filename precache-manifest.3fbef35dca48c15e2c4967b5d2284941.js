6b34ac8fbbd0c8cbbdea8fbdab52953d2669f3a8
Aleksandra <starodub.alexandra@gmail.com>
2019-07-02 16:36:07 +0300

    Updates

---
+++ precache-manifest.3fbef35dca48c15e2c4967b5d2284941.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d9c10fe91e101af693fe799272cc0d",
+    "url": "/dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "fa0ac3adb387cd8e6465",
+    "url": "/dynamic-list-of-posts/static/css/main.40578866.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/dynamic-list-of-posts/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "fa0ac3adb387cd8e6465",
+    "url": "/dynamic-list-of-posts/static/js/main.66eaa7b7.chunk.js"
+  },
+  {
+    "revision": "673aa693b1fbb0657bfb",
+    "url": "/dynamic-list-of-posts/static/js/runtime~main.da5f0574.js"
+  }
+]);
